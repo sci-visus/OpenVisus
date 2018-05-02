344a7face6d29caf69385abce7cf30f610e32655
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-05-02 11:59:20 -0600

    Fixing for llnl

--- ExternalLibs/download-and-compile.sh
+++ ExternalLibs/download-and-compile.sh
@@ -9,7 +9,7 @@ make
 cd .. && rm zlib-1.2.11.tar.gz
 
 # LZ4_INCLUDE_DIR=ExternalLibs/lz4-1.8.1.2/lib
-# LZ4_LIBRARY=ExternalLibs/lz4-1.8.1.2/lib
+# LZ4_LIBRARY=ExternalLibs/lz4-1.8.1.2/lib/libl.so
 wget https://github.com/lz4/lz4/archive/v1.8.1.2.tar.gz
 tar xvzf v1.8.1.2.tar.gz  
 cd lz4-1.8.1.2
@@ -17,17 +17,21 @@ make
 cd .. && rm v1.8.1.2.tar.gz  
 
 # TinyXML_INCLUDE_DIRS=ExternalLibs/tinyxml_2_6_2
-# TinyXML_LIBRARIES=ExternalLibs/tinyxml_2_6_2
+# TinyXML_LIBRARIES=ExternalLibs/tinyxml_2_6_2/libtinyxml.a
 wget https://downloads.sourceforge.net/project/tinyxml/tinyxml/2.6.2/tinyxml_2_6_2.zip
 unzip tinyxml_2_6_2.zip && mv tinyxml tinyxml_2_6_2
 cd tinyxml_2_6_2
-make
+g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 -fPIC   tinyxml.cpp -o tinyxml.o
+g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 -fPIC   tinyxmlparser.cpp -o tinyxmlparser.o
+g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 -fPIC   xmltest.cpp -o xmltest.o
+g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 -fPIC   tinyxmlerror.cpp -o tinyxmlerror.o
+g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 -fPIC   tinystr.cpp -o tinystr.o
 ar rcs libtinyxml.a tinyxml.o tinyxmlparser.o xmltest.o tinyxmlerror.o tinystr.o
 cd .. && rm tinyxml_2_6_2.zip
 
 
 # FREEIMAGE_INCLUDE_DIRS=ExternalLibs/FreeImage3160/Dist
-# FREEIMAGE_LIBRARIES=ExternalLibs/FreeImage3160/Dist
+# FREEIMAGE_LIBRARIES=ExternalLibs/FreeImage3160/Dist/libfreeimage.a
 wget https://sourceforge.net/projects/freeimage/files/Source%20Distribution/3.16.0/FreeImage3160.zip # 3.17 has problems with gcc 5
 unzip FreeImage3160.zip && mv FreeImage FreeImage3160
 cd FreeImage3160
@@ -51,6 +55,18 @@ cd curl-7.59.0
 ./configure
 make
 cd .. && rm curl-7.59.0.tar.gz 
+
+# SWIG_EXECUTABLE=ExternalLibs/swig-3.0.12/install/share/swig/3.0.12
+# SWIG_DIR=ExternalLibs/swig-3.0.12/install/bin/swig
+wget https://downloads.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz
+tar xvzf swig-3.0.12.tar.gz
+cd swig-3.0.12
+./configure --prefix=$(pwd)/install
+make 
+make install
+cd .. && rm swig-3.0.12.tar.gz
+
+
 
 
 
