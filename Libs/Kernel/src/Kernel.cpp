b3ddd5d3b0452660edb38fc007d34bc213e825a7
root <root@molniya.sci.utah.edu>
2018-05-09 12:07:02 -0600

    Fixing linux bug

--- Libs/Kernel/src/Kernel.cpp
+++ Libs/Kernel/src/Kernel.cpp
@@ -278,7 +278,8 @@ static void InitKnownPaths()
         KnownPaths::VisusHome = Path(buff).getChild("visus");
       }
       #else
-        if (auto homedir = getenv("HOME"))
+      {
+	if (auto homedir = getenv("HOME"))
           KnownPaths::VisusHome = Path(homedir).getChild("visus");
 
         else if (auto pw = getpwuid(getuid()))
