0475f9d80624eaf288a7ad201b1698f57ddb2288
scrgiorgio <scrgiorgio@cogesic.it>
2018-08-24 15:48:58 +0200

    Forcing rebuild

--- CMake/setup.py
+++ CMake/setup.py
@@ -2,7 +2,7 @@ import os, sys, setuptools
 import shutil
 
 #increase this number for PIP
-VERSION="1.2.14"
+VERSION="1.2.15"
 
 shutil.rmtree('./build', ignore_errors=True)
 shutil.rmtree('./dist', ignore_errors=True)
