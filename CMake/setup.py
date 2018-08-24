d1e93c3ae05182a16f618447db8eb6acc534ef81
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-08-24 11:06:23 +0200

    Better Python infos at startup

--- CMake/setup.py
+++ CMake/setup.py
@@ -2,7 +2,7 @@ import os, sys, setuptools
 import shutil
 
 #increase this number for PIP
-VERSION="1.2.13"
+VERSION="1.2.14"
 
 shutil.rmtree('./build', ignore_errors=True)
 shutil.rmtree('./dist', ignore_errors=True)
