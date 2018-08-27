bb38a37ddfc9a0adfe72fda4fa1a12db1daae47c
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-08-27 08:46:40 +0200

    1.2.16

--- CMake/setup.py
+++ CMake/setup.py
@@ -2,7 +2,7 @@ import os, sys, setuptools
 import shutil
 
 #increase this number for PIP
-VERSION="1.2.15"
+VERSION="1.2.16"
 
 shutil.rmtree('./build', ignore_errors=True)
 shutil.rmtree('./dist', ignore_errors=True)
