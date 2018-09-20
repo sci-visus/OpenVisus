384c63b9662b39d054ae1529c1371afdea1a6174
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-09-20 18:41:43 +0200

    fixing CI...

--- CMake/setup.py
+++ CMake/setup.py
@@ -2,7 +2,7 @@ import os, sys, setuptools
 import shutil
 
 #increase this number for PIP
-VERSION="1.2.23"
+VERSION="1.2.24"
 
 # ////////////////////////////////////////////////////////////////////
 def cleanAll():
