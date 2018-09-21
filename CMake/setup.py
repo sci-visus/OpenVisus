aae400b9104219e6530e7ed80f03b975f58bd176
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-09-21 08:02:19 +0200

    working...

--- CMake/setup.py
+++ CMake/setup.py
@@ -2,7 +2,7 @@ import os, sys, setuptools
 import shutil
 
 #increase this number for PIP
-VERSION="1.2.26"
+VERSION="1.2.27"
 
 # ////////////////////////////////////////////////////////////////////
 def cleanAll():
