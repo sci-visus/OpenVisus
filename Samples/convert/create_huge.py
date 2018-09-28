358535c91ad1f779c39159c56e745911818d98bd
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-09-28 16:04:28 +0200

    working...

--- Samples/convert/create_huge.py
+++ Samples/convert/create_huge.py
@@ -14,7 +14,7 @@ cmd_env={
 	"PYTHONPATH":build_dir
 }	
 
-Piece=512
+Piece=(1024,1024,1024)
 
 Durl="D:/visus_dataset/2kbit1/huge/lz4/rowmajor/visus.idx"
 Dsize=(8192,8192,16384)
@@ -22,10 +22,13 @@ Dsize=(8192,8192,16384)
 Surl="F:/Google Drive sci.utah.edu/visus_dataset/2kbit1/lz4/rowmajor/visus.idx"
 Ssize=(2048,2048,2048)
 
+blocksperfile=65536
 
 # /////////////////////////////////////
 def executeCommand(cmd):
-	print(cmd)
+	print("")
+	print("//EXECUTING COMMAND....")
+	print(" ".join(cmd))
 	proc=subprocess.Popen(cmd,env=cmd_env)
 	exit_code=proc.wait()
 	if (exit_code!=0):
@@ -34,16 +37,16 @@ def executeCommand(cmd):
 
 if True:
 	fields="DATA uint8[1] format(rowmajor) default_compression(raw)"
-	executeCommand([visus_cmd,"create",Durl,"--box","%d %d %d %d %d %d" % (0,Dsize[0]-1, 0, Dsize[1]-1,0, Dsize[2]-1),"--fields",fields])
+	executeCommand([visus_cmd,"create",Durl,"--blocksperfile",str(blocksperfile),"--box","%d %d %d %d %d %d" % (0,Dsize[0]-1, 0, Dsize[1]-1,0, Dsize[2]-1),"--fields",fields])
 
 if True:
-	for z in range(0,Dsize[2],Piece):
-		for y in range(0,Dsize[1],Piece):
-			for x in range(0,Dsize[0],Piece):
+	for z in range(0,Dsize[2],Piece[2]):
+		for y in range(0,Dsize[1],Piece[1]):
+			for x in range(0,Dsize[0],Piece[0]):
 
-				x1=x; x2=x + Piece - 1
-				y1=y; y2=y + Piece - 1
-				z1=z; z2=z + Piece - 1
+				x1=x; x2=x + Piece[0] - 1
+				y1=y; y2=y + Piece[1] - 1
+				z1=z; z2=z + Piece[2] - 1
 				
 				executeCommand([visus_cmd,
 					"--disable-write-locks",
