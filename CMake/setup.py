d3ec58f2a41c61362fc00ef39d42d6193617fbf0
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-10-03 13:14:44 +0200

    FIxing appveyor/travis

--- CMake/setup.py
+++ CMake/setup.py
@@ -3,7 +3,7 @@ import shutil
 import platform
 
 #increase this number for PIP
-VERSION="1.2.39"
+VERSION="1.2.40"
 
 WIN32=platform.system()=="Windows" or platform.system()=="win32"
 APPLE=platform.system()=="Darwin"
