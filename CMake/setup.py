d451d73f66617d99249ac33b08076637bcf0f036
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-10-03 10:21:06 +0200

    New tag version

--- CMake/setup.py
+++ CMake/setup.py
@@ -3,7 +3,7 @@ import shutil
 import platform
 
 #increase this number for PIP
-VERSION="1.2.35"
+VERSION="1.2.36"
 
 WIN32=platform.system()=="Windows" or platform.system()=="win32"
 APPLE=platform.system()=="Darwin"
