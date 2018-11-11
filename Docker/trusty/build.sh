40e2364865fe4c7cb7ab7ff2832717e883d24f1d
Ubuntu16 <scrgiorgio@gmail.com>
2018-11-11 08:32:23 -0800

    working..

--- Docker/trusty/build.sh
+++ Docker/trusty/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # stop on errors printout commands
 set -ex 
 
@@ -13,29 +12,40 @@ PushArg DISABLE_OPENMP         0
 PushArg VISUS_GUI              1
 PushArg DEPS_INSTALL_DIR       $(pwd)/Linux-x86_64
 PushArg DEPLOY_PYPI            0
+PushArg BUILD_STEP             -1
 
-#  install linux dependencies 
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get -qy update
-sudo apt-get -qy install --allow-unauthenticated cmake swig git bzip2 ca-certificates build-essential libssl-dev uuid-dev
-sudo apt-get -qy install --allow-unauthenticated apache2 apache2-dev 
+#  install linux dependencies
+if [[ $BUILD_STEP==0 || $BUILD_STEP==-1 ]]; then
+	sudo add-apt-repository -y ppa:deadsnakes/ppa
+	sudo apt-get -qy update
+	sudo apt-get -qy install --allow-unauthenticated cmake swig git bzip2 ca-certificates build-essential libssl-dev uuid-dev patchelf
+	sudo apt-get -qy install --allow-unauthenticated apache2 apache2-dev 
+fi
 
-InstallPyEnv                    $HOME
-InstallPatchElfFromSource       $DEPS_INSTALL_DIR
-InstallPrecompiledCMakeForLinux $DEPS_INSTALL_DIR
+# install pyEnv
+if [[ $BUILD_STEP==1 || $BUILD_STEP==-1 ]]; then
+	InstallPyEnv $HOME
+fi
 
 # install dependencies
-if (( VISUS_INTERNAL_DEFAULT==0 )); then 
-  sudo apt-get install zlib1g-dev liblz4-dev libtinyxml-dev libfreeimage-dev libssl-dev libcurl4-openssl-dev
+if [[ $BUILD_STEP==2 || $BUILD_STEP==-1 ]]; then
+	if (( VISUS_INTERNAL_DEFAULT==0 )); then 
+	  sudo apt-get install zlib1g-dev liblz4-dev libtinyxml-dev libfreeimage-dev libssl-dev libcurl4-openssl-dev
+	fi
 fi
-
+	
 # install qt 
-if ((VISUS_GUI==1)); then
-	InstallQtForUbuntu 
+if [[ $BUILD_STEP==3 || $BUILD_STEP==-1 ]]; then
+	if ((VISUS_GUI==1)); then
+		InstallQtForUbuntu 
+	fi
 fi
 
-SetupCmakeOptions()
-Build()
+# compile install openvisus
+if [[ $BUILD_STEP==4 || $BUILD_STEP==-1 ]]; then
+	SetupCmakeOptions()
+	Build()
+fi
 
 
 
