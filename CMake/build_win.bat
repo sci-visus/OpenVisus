abbfd9f4b65e4e6bfc46bd58ef4178460943504c
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-11-16 13:15:38 -0700

    Testing [full_matrix] [deploy_github] [deploy_pypi]

--- CMake/build_win.bat
+++ CMake/build_win.bat
@@ -1,3 +1,4 @@
+@echo on
 
 REM setup compiler 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
@@ -29,11 +30,13 @@ cmake --build . --target INSTALL       --config RelWithDebInfo
 cmake --build . --target deploy        --config RelWithDebInfo
 
 if %DEPLOY_GITHUB%==1 (
+   echo "Deploying to github enabled"
    cmake --build . --target sdist --config RelWithDebInfo
    "%PYTHON_EXECUTABLE%" -c "import os;import glob;filename=glob.glob('install/dist/*.zip')[0];os.rename(filename,filename.replace('.zip','.win_amd64.zip'))" 
 )
  
 if %DEPLOY_PYPI%==1 (
+   echo "Deploying to pypi enabled"
    cmake --build . --target bdist_wheel   --config RelWithDebInfo
    cmake --build . --target pypi          --config RelWithDebInfo
 )
