2ce36dad42a8d6ace848c56ea08dd86d08ccdc16
scrgiorgio <scrgiorgio@cogesic.it>
2018-12-11 10:39:32 +0100

    Fixing linux

--- CMake/visus.sh
+++ CMake/visus.sh
@@ -5,6 +5,7 @@ this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 cd ${this_dir}
 
 export LD_LIBRARY_PATH=$(python -c "import os,sysconfig; print(os.path.realpath(sysconfig.get_config_var('LIBDIR')))")
-export PYTHONPATH=$(pwd)
+
+export PYTHONPATH=$(pwd):$(python -c "import sys; print(':'.join(sys.path))")
 
 ./bin/visus "$@"
