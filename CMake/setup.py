ae3684a8125e8d0e81ce96a80e3ccd49c56f2e09
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-12-13 16:46:55 +0100

    New tag

--- CMake/setup.py
+++ CMake/setup.py
@@ -8,7 +8,7 @@ import atexit
 import setuptools
 from configure import *
 	
-PROJECT_VERSION="1.2.137"
+PROJECT_VERSION="1.2.138"
 PROJECT_NAME="OpenVisus"
 PROJECT_URL="https://github.com/sci-visus/OpenVisus"
 PROJECT_DESCRIPTION="ViSUS multiresolution I/O, analysis, and visualization system"
