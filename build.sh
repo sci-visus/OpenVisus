a9c200709d50b5a9eb1a14114f391d90f22a7c81
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-12-20 21:10:08 +0100

    working

--- build.sh
+++ build.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
-set -ex 
 
 this_dir=$(dirname $0)
 
-if [ -n "${DOCKER_IMAGE}" ]; then
-
-	DOCKER_SHELL=${DOCKER_SHELL:-/bin/bash}
-	DOCKER_ENV=${DOCKER_ENV:-}
-	BUILD_DIR=${BUILD_DIR:-build}
-
-	if [ "${PYTHON_VERSION}"         != "" ] ; then DOCKER_ENV="${DOCKER_ENV} -e PYTHON_VERSION=${PYTHON_VERSION}" ; fi
-	if [ "${VISUS_INTERNAL_DEFAULT}" != "" ] ; then DOCKER_ENV="${DOCKER_ENV} -e VISUS_INTERNAL_DEFAULT=${VISUS_INTERNAL_DEFAULT}" ; fi
-	if [ "${BUILD_DIR}"              != "" ] ; then DOCKER_ENV="${DOCKER_ENV} -e BUILD_DIR=${BUILD_DIR}" ; fi
-
-	sudo docker rm -f mydocker || true
-	sudo docker run -d -ti --name mydocker -v $(pwd):/home/OpenVisus ${DOCKER_ENV} ${DOCKER_IMAGE} ${DOCKER_SHELL}
-	sudo docker exec mydocker ${DOCKER_SHELL} -c "cd /home/OpenVisus && ./build.sh"
-
-	sudo chown -R "$USER":"$USER" ${BUILD_DIR}
-	sudo chmod -R u+rwx           ${BUILD_DIR}
-
-elif [ $(uname) = "Darwin" ]; then
+if [ $(uname) = "Darwin" ]; then
 	${this_dir}/CMake/build_osx.sh
 
 elif [ -x "$(command -v apt-get)" ]; then
