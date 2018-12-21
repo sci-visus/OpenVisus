a5c7cf114000ecde36d72c7d193db3c116350f76
Giorgio Scorzelli <scrgiorgio@gmail.com>
2018-12-21 08:58:47 +0100

    working

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ if [ -n "${DOCKER_IMAGE}" ]; then
 	if [ "${BUILD_DIR}"              != "" ] ; then DOCKER_ENV="${DOCKER_ENV} -e BUILD_DIR=${BUILD_DIR}" ; fi
 
 	sudo docker rm -f mydocker || true
-	sudo docker run -d -ti --name mydocker -v $(pwd):/home/OpenVisus ${DOCKER_ENV} ${DOCKER_IMAGE} ${DOCKER_SHELL}
-	sudo docker exec mydocker ${DOCKER_SHELL} -c "cd /home/OpenVisus && ./build.sh"
+	sudo docker run -d -ti --name mydocker -v ${this_dir}:${this_dir} ${DOCKER_ENV} ${DOCKER_IMAGE} ${DOCKER_SHELL}
+	sudo docker exec mydocker ${DOCKER_SHELL} -c "cd ${this_dir} && ./build.sh"
 
 	sudo chown -R "$USER":"$USER" ${BUILD_DIR}
 	sudo chmod -R u+rwx           ${BUILD_DIR}
