284a583d1760406cceb70c77582d96af99eadfeb
spetruzza <steve.petruzza@gmail.com>
2019-01-17 18:08:10 -0700

    new docker visus dataportal

---
+++ Docker/dataportal/local.php
@@ -0,0 +1,19 @@
+<?php 
+  // This is a local site configuration file
+	
+  // Admin credentials for this server
+  $admin_user="admin"; 
+  $admin_password="password";
+	
+  // Default server config file location (on the server's filesystem)	
+  $default_config_file="/home/visus/visus.config";
+  
+  // Data directory
+  $data_dir="/data";
+    
+  // visus executable
+  $visus_exe="/usr/local/lib/python3.5/dist-packages/OpenVisus/bin/visus";
+  
+  // mod_visus_url
+  $mod_visus_url="http://localhost:8080/mod_visus?";
+?>
