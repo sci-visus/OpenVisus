2253209e72496d36fc6da4b50070f1142acaf04d
Giorgio Scorzelli <scrgiorgio@gmail.com>
2019-02-01 11:03:27 +0100

    new tag

--- CMake/setup.py
+++ CMake/setup.py
@@ -9,7 +9,7 @@ from configure import *
 	
 # git grep -l 'old' | xargs sed -i 's/old/new/g'
 PROJECT_NAME="OpenVisus"
-PROJECT_VERSION="1.2.184"
+PROJECT_VERSION="1.2.185"
 
 # ////////////////////////////////////////////////////////////////////
 def findFilesInCurrentDirectory():
