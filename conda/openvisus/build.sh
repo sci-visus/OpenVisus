d98022235b4dad07f4de33ac49ef5482707922b6
scrgiorgio <scrgiorgio@cogesic.it>
2019-02-05 20:37:32 +0100

    fixing travis

--- conda/openvisus/build.sh
+++ conda/openvisus/build.sh
@@ -16,21 +16,27 @@ mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
 __cmake_options__=""
+
+# XCode for Darwin doesn't work
+if [ $(uname) = "Darwin" ]; then
+	__cmake_options__+=" -DCMAKE_C_COMPILER=clang"
+	__cmake_options__+=" -DCMAKE_CXX_COMPILER=clang++"
+fi
+
 __cmake_options__+=" -DDISABLE_OPENMP=1"
 __cmake_options__+=" -DVISUS_GUI=0"
 __cmake_options__+=" -DVISUS_INTERNAL_DEFAULT=1"
 __cmake_options__+=" -DPYTHON_VERSION=${PY_VER}"
 __cmake_options__+=" -DPYTHON_EXECUTABLE=${PYTHON}"
 
-if [ $(uname) = "Darwin" ]; then
-	cmake -GXcode ${__cmake_options__} ${SOURCE_DIR}
-	cmake --build ./ --target ALL_BUILD --config ${CMAKE_BUILD_TYPE}
-	cmake --build ./ --target install   --config ${CMAKE_BUILD_TYPE} 	
-else
-	cmake ${__cmake_options__} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${SOURCE_DIR}
-	cmake --build ./ --target all -- -j 4
-	cmake --build ./ --target install
-fi
+# having problems with some samples...
+__cmake_options__+=" -DVISUS_TEST=OFF"
+
+cmake ${__cmake_options__} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${SOURCE_DIR}
+
+cmake --build ./ --target all -- -j 4
+
+cmake --build ./ --target install  
 
 pushd install
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
