5d4e166470223ffaa173ed4f18e4aa8de5d839bc
Giorgio Scorzelli <scrgiorgio@gmail.com>
2019-02-05 16:47:09 +0100

    fixing build

--- conda/openvisus/build.sh
+++ conda/openvisus/build.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
+set -ex
+
+SOURCE_DIR=$(pwd)
+BUILD_DIR=${BUILD_DIR:-$SOURCE_DIR/build}
 CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-RelWithDebInfo}
 
-mkdir -p build
-cd build
+echo "SOURCE_DIR        ${SOURCE_DIR}"
+echo "BUILD_DIR         ${BUILD_DIR}"
+echo "CMAKE_BUILD_TYPE  ${CMAKE_BUILD_TYPE}"
+echo "PY_VER            ${PY_VER}"
+echo "PYTHON            ${PYTHON}"
+
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+__cmake_options__=""
+__cmake_options__+=" -DDISABLE_OPENMP=1"
+__cmake_options__+=" -DVISUS_GUI=0"
+__cmake_options__+=" -DVISUS_INTERNAL_DEFAULT=1"
+__cmake_options__+=" -DPYTHON_VERSION=${PY_VER}"
+__cmake_options__+=" -DPYTHON_EXECUTABLE=${PYTHON}"
 
 if [ $(uname) = "Darwin" ]; then
-
-	cmake -GXcode \
-		-DDISABLE_OPENMP=1 \
-		-DVISUS_GUI=0 \
-		-DVISUS_INTERNAL_DEFAULT=1 \
-		-DPYTHON_VERSION=$PY_VER \
-		-DPYTHON_EXECUTABLE=$PYTHON \
-		../
-	
+	cmake -GXcode ${__cmake_options__} ${SOURCE_DIR}
 	cmake --build ./ --target ALL_BUILD --config ${CMAKE_BUILD_TYPE}
-	cmake --build ./ --target install   --config ${CMAKE_BUILD_TYPE} 		
-		
+	cmake --build ./ --target install   --config ${CMAKE_BUILD_TYPE} 	
 else
-
-	cmake \
-		-DDISABLE_OPENMP=1 \
-		-DVISUS_GUI=0 \
-		-DVISUS_INTERNAL_DEFAULT=1 \
-		-DPYTHON_VERSION=$PY_VER \
-		-DPYTHON_EXECUTABLE=$PYTHON \	
-		-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \	
-		../
-		
+	cmake ${__cmake_options__} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${SOURCE_DIR}
 	cmake --build ./ --target all -- -j 4
 	cmake --build ./ --target install
-	
 fi
 
 pushd install
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 popd
+
+set +ex
