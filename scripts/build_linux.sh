1eab4d380d84281c117a5d9ec1e082fd52f2c2f5
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-05-10 12:57:50 +0200

    Test

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -14,10 +14,12 @@ cd Release/OpenVisus
 PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus test
 PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus convert
 
-GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
-if [[ "${GIT_TAG}" != "" && "${PYPI_USERNAME}" != "" && "${PYPI_PASSWORD}" != "" ]] ; then
+
 	${Python_EXECUTABLE} -m pip install setuptools wheel --upgrade || true
 	${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1} --plat-name=manylinux2010_x86_64
+
+GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
+if [[ "${GIT_TAG}" != "" && "${PYPI_USERNAME}" != "" && "${PYPI_PASSWORD}" != "" ]] ; then
 	${Python_EXECUTABLE} -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} --skip-existing dist/OpenVisus-*.whl
 fi
 
