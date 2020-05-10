6763fdf98ede3675dd378a79a466f17a8278655a
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-05-10 13:03:46 +0200

    Test

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,8 +3,12 @@
 set -e  # stop or error
 set -x  # very verbose
 
+${Python_EXECUTABLE} -m pip install numpy setuptools wheel twine --upgrade 1>/dev/null 
+
 mkdir -p build 
 cd build
+
+
 cmake -DPython_EXECUTABLE=${Python_EXECUTABLE} -DQt5_DIR=${Qt5_DIR} ../
 cmake --build ./ --target all     --config Release --parallel 4
 cmake --build ./ --target install --config Release
@@ -14,9 +18,8 @@ cd Release/OpenVisus
 PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus test
 PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus convert
 
-
-	${Python_EXECUTABLE} -m pip install setuptools wheel --upgrade || true
-	${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1} --plat-name=manylinux2010_x86_64
+${Python_EXECUTABLE} -m pip install setuptools wheel --upgrade || true
+${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1} --plat-name=manylinux2010_x86_64
 
 GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
 if [[ "${GIT_TAG}" != "" && "${PYPI_USERNAME}" != "" && "${PYPI_PASSWORD}" != "" ]] ; then
