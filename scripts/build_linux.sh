fce8041455dc4feb41d0b8711defa040540250b8
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-05-10 13:06:33 +0200

    Test

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,11 +3,10 @@
 set -e  # stop or error
 set -x  # very verbose
 
-${Python_EXECUTABLE} -m pip install numpy setuptools wheel twine --upgrade 1>/dev/null 
+${Python_EXECUTABLE} -m pip install numpy setuptools wheel twine --upgrade 1>/dev/null || true
 
 mkdir -p build 
 cd build
-
 
 cmake -DPython_EXECUTABLE=${Python_EXECUTABLE} -DQt5_DIR=${Qt5_DIR} ../
 cmake --build ./ --target all     --config Release --parallel 4
@@ -18,12 +17,12 @@ cd Release/OpenVisus
 PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus test
 PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus convert
 
-${Python_EXECUTABLE} -m pip install setuptools wheel --upgrade || true
-${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1} --plat-name=manylinux2010_x86_64
-
 GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
 if [[ "${GIT_TAG}" != "" && "${PYPI_USERNAME}" != "" && "${PYPI_PASSWORD}" != "" ]] ; then
-	${Python_EXECUTABLE} -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} --skip-existing dist/OpenVisus-*.whl
+   PLATFORM_NAME=manylinux2010_x86_64
+   PLATFORM_TAG=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}     
+	 ${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=${PLATFORM_TAG} --plat-name=${PLATFORM_NAME}
+	 ${Python_EXECUTABLE} -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} --skip-existing dist/OpenVisus-*.whl
 fi
 
 
