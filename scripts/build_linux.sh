26cb720b804cbf1bd05656641e72ec4959bca64c
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-05-11 09:37:31 +0200

    Test

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,7 +5,7 @@ set -x  # very verbose
 
 PYTHON_VERSION=${PYTHON_VERSION:-3.6}
 Python_EXECUTABLE=$(which python${PYTHON_VERSION})
-Qt5_DIR=/opt/qt59
+Qt5_DIR=${Qt5_DIR:-/opt/qt59}
 
 ${Python_EXECUTABLE} -m pip install numpy setuptools wheel twine --upgrade 1>/dev/null || true
 
@@ -23,12 +23,8 @@ PYTHONPATH=../ ${Python_EXECUTABLE} -m OpenVisus convert
 
 GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
 if [[ "${GIT_TAG}" != "" && "${PYPI_USERNAME}" != "" && "${PYPI_PASSWORD}" != "" ]] ; then
-   PLATFORM_NAME=manylinux2010_x86_64
-   PLATFORM_TAG=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}     
-	 ${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=${PLATFORM_TAG} --plat-name=${PLATFORM_NAME}
+	 ${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=cp${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1} --plat-name=manylinux2010_x86_64
 	 ${Python_EXECUTABLE} -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} --skip-existing dist/OpenVisus-*.whl
 fi
-
-
 
 
