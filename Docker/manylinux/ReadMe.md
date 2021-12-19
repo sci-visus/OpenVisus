bdc34a274a63a11881173ab9d7571e5d7982ea52
Giorgio Scorzelli <scrgiorgio@gmail.com>
2021-12-19 08:29:23 +0100

    working...

--- Docker/manylinux/ReadMe.md
+++ Docker/manylinux/ReadMe.md
@@ -6,12 +6,33 @@ Run (under Windows CLI, to adapt for other OS):
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker login -u scrgiorgio
 # type the password
-docker buildx create --name mybuilder --use || true
-docker buildx build --platform linux/amd64 -t visus/manylinux:1.0 -t visus/manylinux:latest --progress=plain -f Dockerfile.x86_64  ./ --push
+sudo docker buildx create --name mybuilder --use || true
+sudo docker buildx build --platform linux/amd64 -t visus/manylinux:1.0 -t visus/manylinux:latest --progress=plain -f Dockerfile.x86_64  ./ --push
 
 docker buildx build --platform linux/arm64 -t visus/manylinux:1.0 -t visus/manylinux:latest --progress=plain -f Dockerfile.aarch64 ./ --push
 ```
 
-
-
 docker run --platform linux/arm64 -it --rm quay.io/pypa/manylinux2014_aarch64 bash
+
+# AARCH 64
+
+```
+docker run -it --rm  -v ${PWD}:${PWD} -w ${PWD} -e PYTHON_VERSION=3.8 -e VISUS_GUI=0 -e VISUS_SLAM=0 -e VISUS_MODVISUS=0 continuumio/miniconda3:latest bash -c '
+conda create --name my-python -y python=${PYTHON_VERSION}
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate my-python
+conda install -y numpy anaconda-client conda conda-build wheel   make cmake patchelf swig
+conda install -y -c conda-forge cxx-compiler
+
+if [[ "${VISUS_GUI}"=="1"]]; then
+	conda install -c conda-forge -y qt==5.12.9
+fi
+
+Python_EXECUTABLE=`which python` BUILD_DIR=build_conda ./scripts/build_linux.sh
+
+apt-get update
+apt-get install -y vim
+disable visus executable..
+'
+
+
--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-set -e  # stop or error
-set -x  # very verbose
+set -e
+set -x
 
+# settings
 BUILD_DIR=${BUILD_DIR:-$(pwd)/build}
 VISUS_GUI=${VISUS_GUI:-1}
 VISUS_MODVISUS=${VISUS_MODVISUS:-1}
 VISUS_SLAM=${VISUS_SLAM:-1}
 Python_EXECUTABLE=${Python_EXECUTABLE:-python} 
-
 GIT_TAG=$(git describe --tags --exact-match 2>/dev/null || true)
 OPENVISUS_TAG=`${Python_EXECUTABLE} Libs/swig/setup.py print-tag`
 PYTHON_VERSION=`${Python_EXECUTABLE} -c "import sys;print(f'{sys.version_info.major}{sys.version_info.minor}')"`
 
+# compile
 mkdir -p ${BUILD_DIR}
-cd ${BUILD_DIR} 
-
+pushd ${BUILD_DIR} 
 cmake \
 	-DPython_EXECUTABLE=${Python_EXECUTABLE} \
 	-DQt5_DIR=${Qt5_DIR} \
@@ -23,43 +23,42 @@ cmake \
 	-DVISUS_MODVISUS=${VISUS_MODVISUS} \
 	-DVISUS_SLAM=${VISUS_SLAM} \
 	../
-
 cmake --build ./ --target all     --config Release --parallel 4
-
 cmake --build ./ --target install --config Release
+popd
 
-export PYTHONPATH=./Release
-
+# test
+export PYTHONPATH=${BUILD_DIR}/Release
 ${Python_EXECUTABLE} -m OpenVisus configure || true # segmentation fault problem
-${Python_EXECUTABLE}  -m OpenVisus test
-
-# this is just to check the linking, not producing anything
+${Python_EXECUTABLE} -m OpenVisus test
 if [[ "${VISUS_GUI}" == "1" ]] ; then 
-	${Python_EXECUTABLE}  -m OpenVisus test-gui 
+	${Python_EXECUTABLE}  -m OpenVisus test-gui  # this is just to check the linking, not producing anything
 fi
+unset PYTHONPATH
 
-# create the wheel and upload
-if [[ "${GIT_TAG}" != "" && "${PYPI_USERNAME}"!="" && "${PYPI_PASSWORD}"!="" && "${PYPI_PLATFORM_NAME}"!="" ]] ; then
-	pushd build/Release/OpenVisus
+# PyPi
+if [[ "${GIT_TAG}"!="" && "${PYPI_USERNAME}"!="" && "${PYPI_PASSWORD}"!=""]] ; then
+	pushd ${BUILD_DIR}/Release/OpenVisus
 	${Python_EXECUTABLE} -m pip install -q setuptools wheel twine
 	${Python_EXECUTABLE} setup.py -q bdist_wheel --python-tag=cp${PYTHON_VERSION} --plat-name==${PYPI_PLATFORM_NAME}
 	${Python_EXECUTABLE} -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_PASSWORD} --skip-existing  "dist/*.whl" 
 	popd
-fi
- 
-# for python version see Docker file (it's 3.7 on http:2.4 / debian:buster-slim)
-if [[ "${GIT_TAG}" != "" && "${PYTHON_VERSION}" == "37" && "${DOCKER_TOKEN}" != "" && "${DOCKER_USERNAME}" != "" ]] ; then
-	pushd Docker/mod_visus
-	sleep 30 # give time pypi to get the pushed file
-	docker build --tag visus/mod_visus:$OPENVISUS_TAG --tag visus/mod_visus:latest --build-arg OPENVISUS_TAG=${OPENVISUS_TAG} .
-	echo ${DOCKER_USERNAME} | docker login -u=${DOCKER_TOKEN} --password-stdin
-	docker push visus/mod_visus:$OPENVISUS_TAG
-	docker push visus/mod_visus:latest
- popd
+	
+	# Docker visus/mod_visus
+	if [[ "${DOCKER_USERNAME}"!="" && "${DOCKER_TOKEN}"!="" ]] ; then 
+		sleep 30 # give time pypi to get the pushed file
+		pushd Docker/mod_visus
+		docker build --tag visus/mod_visus:$OPENVISUS_TAG --tag visus/mod_visus:latest --build-arg OPENVISUS_TAG=${OPENVISUS_TAG} .
+		echo ${DOCKER_USERNAME} | docker login -u=${DOCKER_TOKEN} --password-stdin
+		docker push visus/mod_visus:$OPENVISUS_TAG
+		docker push visus/mod_visus:latest	
+		popd
+	fi
 fi
 
-if [[ "${GIT_TAG}" != "" && "${ANACONDA_TOKEN}" != ""  ]] ; then
-	pushd build/Release/OpenVisus
+# conda
+if [[ "${GIT_TAG}"!="" && "${ANACONDA_TOKEN}" != ""  ]] ; then
+	pushd ${BUILD_DIR}/Release/OpenVisus
 	conda install --yes anaconda-client conda-build wheel 1>/dev/null || true
 	rm -Rf $(find ${CONDA_PREFIX} -iname "openvisus*.tar.bz2")  || true
 	python setup.py -q bdist_conda 1>/dev/null
