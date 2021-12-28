3223576fb1ea4452dc34e489a7edfd768f04b6d0
Giorgio Scorzelli <scrgiorgio@gmail.com>
2021-12-28 18:28:53 +0100

    working...

--- scripts/build_windows.conda.sh
+++ scripts/build_windows.conda.sh
@@ -22,20 +22,47 @@ BUILD_DIR=build
 
 mkdir -p ${BUILD_DIR} 
 cd ${BUILD_DIR}
-cmake -G "Visual Studio 16 2019" -A x64 -DQt5_DIR=${CONDA_PREFIX}/Library/lib/cmake/Qt5 -DPython_EXECUTABLE=${PYTHON} -DSWIG_EXECUTABLE=../swigwin-4.0.2/swig.exe ../ \
-    -DVISUS_GUI=0 -DVISUS_SLAM=0 # !!!REMOVE !!!!
+cmake -G "Visual Studio 16 2019" -A x64 -DQt5_DIR=${CONDA_PREFIX}/Library/lib/cmake/Qt5 -DPython_EXECUTABLE=${PYTHON} -DSWIG_EXECUTABLE=../swigwin-4.0.2/swig.exe \
+    -DVISUS_GUI=0 -DVISUS_SLAM=0 -DVISUS_MODVISSU=0 \
+    ../ 
 cmake --build . --target ALL_BUILD --config Release --parallel 4
-cmake --build . --target install	 --config Release
+cmake --build . --target install   --config Release
 
 CreateNonGuiVersion
 
 # ////////////////////////////////////////////////////////////// !!!REMOVE !!!!
 function DistribToConda() {
-    rm -Rf $(find ${CONDA_PREFIX} -iname "openvisus*.tar.bz2")  || true 
-    $PYTHON -v setup.py  bdist_conda
+
+    if [[ ! -f QT_VERSION ]] ; then 
+        PACKAGE_NAME=openvisusnogui
+    else
+        PACKAGE_NAME=openvisus
+    fi
+
+    # The -f option forces a reinstall if a previous version of the package was already installed.
+    cat <<EOF > meta.yaml
+package:
+    name: $PACKAGE_NAME
+    version: "$GIT_TAG"
+source:
+    path: .
+build:
+    script: python setup.py install -f 
+requirements:
+    build:
+        - python
+        - setuptools
+    run:
+        - python
+EOF
+
+    rm -Rf $(find ${CONDA_PREFIX} -iname "openvisus*.tar.bz2")  || true \
+    # 
+    # $PYTHON -v setup.py  bdist_conda (BROKEN, always problems with bdist_conda)
+    conda build .
     CONDA_FILENAME=`find ${CONDA_PREFIX} -iname "openvisus*.tar.bz2"    | head -n 1` 
     if [[ "${GIT_TAG}" != ""    ]] ; then
-        anaconda --verbose --show-traceback -t ${ANACONDA_TOKEN}     upload ${CONDA_FILENAME}
+        anaconda --verbose --show-traceback -t ${ANACONDA_TOKEN} upload ${CONDA_FILENAME}
     fi
 }       
 
