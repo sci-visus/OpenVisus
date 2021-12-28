bf6678de1f765af662bf5a4c63b76afae06e0ca0
Giorgio Scorzelli <scrgiorgio@gmail.com>
2021-12-28 15:34:10 +0100

    working...

--- scripts/build_windows.conda.sh
+++ scripts/build_windows.conda.sh
@@ -9,12 +9,18 @@ source scripts/build_utils.sh
 curl -L --insecure https://cfhcable.dl.sourceforge.net/project/swig/swigwin/swigwin-4.0.2/swigwin-4.0.2.zip -O 
 unzip swigwin-4.0.2.zip
 
-# install ospray
-git clone https://github.com/sci-visus/ospray_win.git ExternalLibs/ospray_win
+# (DISABLED) install ospray
+# git clone https://github.com/sci-visus/ospray_win.git ExternalLibs/ospray_win
 
 # configure conda
 conda config --set always_yes yes --set changeps1 no --set anaconda_upload no             1>/dev/null
 conda install -c conda-forge -y conda cmake pyqt=5.12  anaconda-client  conda-build wheel 1>/dev/null
+
+find $(wslpath -u $CONDA_PREFIX)
+
+cp -n \
+    $(wslpath -u $CONDA_PREFIX)/Lib/distutils/command/bdist_conda.py \
+    $(wslpath -u $CONDA_PREFIX)/Lib/site-packages/setuptools/bdist_conda.py
 
 GIT_TAG=`git describe --tags --exact-match 2>/dev/null || true`
 PYTHON=`which python`
@@ -30,15 +36,11 @@ CreateNonGuiVersion
 
 pushd Release/OpenVisus
 ConfigureAndTestConda
-
-find CONDA_PREFIX/Lib
-cp -n $CONDA_PREFIX/Lib/distutils/command/bdist_conda.py $CONDA_PREFIX/Lib/site-packages/setuptools/bdist_conda.py
 DistribToConda
 popd
 
 pushd Release.nogui/OpenVisus
 ConfigureAndTestConda
-cp -n $CONDA_PREFIX/Lib/distutils/command/bdist_conda.py $CONDA_PREFIX/Lib/site-packages/setuptools/bdist_conda.py
 DistribToConda
 popd
 
