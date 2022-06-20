435b1c77da0009398e553c7f57be1d85089388d6
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-06-20 20:38:52 +0200

    Disabling GUI for linux

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -2,8 +2,16 @@
 
 set -ex
 
+
+
+
 BUILD_DIR=${BUILD_DIR:-build_docker}
-VISUS_GUI=${VISUS_GUI:-1}
+
+# scrgiorgio 20 June 2022: temporary disabling GUI, getting PyQt5 error
+# VISUS_GUI=${VISUS_GUI:-1}
+VISUS_GUI=0
+
+
 VISUS_SLAM=${VISUS_SLAM:-1}
 VISUS_MODVISUS=${VISUS_MODVISUS:-1}
 PYTHON_VERSION=${PYTHON_VERSION:-3.8}
@@ -152,7 +160,7 @@ if [[ "1" == "1" ]]; then
   mkdir -p ${BUILD_DIR} 
   cd ${BUILD_DIR}
   cmake -DPython_EXECUTABLE=${PYTHON} -DQt5_DIR=${Qt5_DIR} -DVISUS_GUI=${VISUS_GUI} -DVISUS_MODVISUS=${VISUS_MODVISUS} -DVISUS_SLAM=${VISUS_SLAM} ../
-  make -j 
+  make -j
   make install
 fi
 
