18cd6ae03097ed184c85e672210d8a1905667aa3
Aashish <aashishpanta0@gmail.com>
2022-10-06 23:39:09 -0600

    New tag (2.1.242)

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -109,12 +109,10 @@ function DistribToPip() {
   rm -Rf ./dist
   $PYTHON -m pip install --upgrade pip || true 
   $PYTHON -m pip install setuptools wheel cryptography==3.4.0 twine || true
-  $PYTHON -m pip install setuptools wheel cryptography==3.4.0 twine || true
   
-
   PYTHON_TAG=cp$(echo $PYTHON_VERSION | awk -F'.' '{print $1 $2}')
   $PYTHON setup.py -q bdist_wheel --python-tag=$PYTHON_TAG --plat-name=$PIP_PLATFORM
-  $PYTHON -m pip install --upgrade twine || true
+  
   if [[ "${GIT_TAG}" != "" ]] ; then
     $PYTHON -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_TOKEN} --skip-existing   "dist/*.whl" 
   fi
@@ -147,7 +145,7 @@ function DistribToConda() {
 PYTHON=`which python${PYTHON_VERSION}`
 
 # this is for linux/docker (is this needed?)
-# yum install -y libffi-devel
+yum install -y libffi-devel
 
 # make sure pip is updated
 ${PYTHON} -m pip install --upgrade pip || true
