bfffe924b0c534d83ae05d3ceecccdb3a62754b3
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-12-02 10:14:36 -0700

    Ading some python utilities

--- Libs/swig/utils.py
+++ Libs/swig/utils.py
@@ -1,4 +1,10 @@
-import os,sys,glob,threading,platform,sysconfig,re,time,subprocess, errno, fnmatch, shutil
+import os,sys,glob,threading,platform,sysconfig,re,time,subprocess, errno, fnmatch, shutil, logging
+import traceback, time, subprocess, shutil, shlex, math
+import io, queue, threading, json, multiprocessing,functools, urllib3, configparser
+from   urllib.parse import parse_qs, urlparse
+import csv,yaml
+
+logger = logging.getLogger("OpenVisus")
 
 # ////////////////////////////////////////////////////////////////////////////////
 def ThisDir(file):
@@ -23,6 +29,13 @@ def CreateDirectory(value):
 		if not os.path.isdir(value):
 			raise
 	
+# /////////////////////////////////////////////////////////////////////////
+def MakeDirForFile(filename):
+	try:
+		os.makedirs(os.path.dirname(filename),exist_ok=True)
+	except:
+		pass
+
 # /////////////////////////////////////////////////////////////////////////
 def GetFilenameWithoutExtension(filename):
 	return os.path.splitext(os.path.basename(filename))[0]
@@ -158,39 +171,82 @@ def ExecuteCommand(cmd):
 	return subprocess.call(cmd, shell=False)
 
 
+
+# //////////////////////////////////////////////////////////////////////////////
+def WriteCSV(filename,rows):
+	with open(filename,"wt") as f:
+		writer=csv.writer(f)
+		writer.writerows([row for row in rows if row])
+
+# //////////////////////////////////////////////////////////////////////////////
+def ReadCSV(filename):
+	with open(filename,"rt") as f:
+		reader=csv.reader(f)
+		return [row for row in reader if row]
+
+# //////////////////////////////////////////////////////////////////////
+def WriteYaml(filename,data):
+	logger.info(f"Writing yaml {filename}...")
+	with open(filename, 'w') as stream:
+		yaml.dump(data, stream)
+	logger.info(f"Writing yaml {filename} DONE")
+
+# //////////////////////////////////////////////////////////////////////
+def ReadYaml(filename):
+	logger.info(f"Reading yaml {filename}...")
+	with open(filename, 'r') as stream:
+		ret=yaml.load(stream, Loader=yaml.CLoader)
+	logger.info(f"Reading yaml {filename} DONE")
+	return ret
+
+# /////////////////////////////////////////////////////////////////////////
+def HumanSize(size):
+	KiB,MiB,GiB,TiB=1024,1024*1024,1024*1024*1024,1024*1024*1024*1024
+	if size>TiB: return "{:.2f}TiB".format(size/TiB) 
+	if size>GiB: return "{:.2f}GiB".format(size/GiB) 
+	if size>MiB: return "{:.2f}MiB".format(size/MiB) 
+	if size>KiB: return "{:.2f}KiB".format(size/KiB) 
+	return str(size)
+
+# ////////////////////////////////////////////////////////////////
+def SetupLogger(logger, output_stdout:bool=True, log_filename:str=None, logging_level=logging.INFO):
+
+	logger.setLevel(logging_level)
+
+	# stdout
+	if output_stdout:
+		handler=logging.StreamHandler()
+		handler.setLevel(logging_level)
+		handler.setFormatter(logging.Formatter(fmt=f"[%(asctime)s][%(levelname)s][%(name)s] %(message)s", datefmt="%H%M%S"))
+		logger.addHandler(handler)
+	
+	# file
+	if log_filename:
+		os.makedirs(os.path.dirname(log_filename),exist_ok=True)
+		handler=logging.FileHandler(log_filename)
+		handler.setLevel(logging_level)
+		handler.setFormatter(logging.Formatter(fmt=f"[%(asctime)s][%(levelname)s][%(name)s] %(message)s", datefmt="%H%M%S"))
+		logger.addHandler(handler)
+
+
 # ///////////////////////////////////////////////////////////////////////
-def RunJobsInParallel(jobs, advance_callback=None, nthreads=8):
+# deprecated: use python multiprocessing ThreadPool map or map_async
+def RunJobsInParallel(jobs, advance_callback=None, nthreads=8, timeout=60*60*24*30):
+	from multiprocessing.pool import ThreadPool
+	p=ThreadPool(nthreads)
+	chunk_size,N=1,len(jobs)
+	return p.map_async(jobs, []*len(jobs), chunk_size).get(timeout=timeout) 
 
-	class MyThread(threading.Thread):
 
-		# constructor
-		def __init__(self):
-			super(MyThread, self).__init__()
-
-		# run
-		def run(self):
-			for job in self.jobs:
-				self.jobDone(job())
-
-	nthreads=min(nthreads,len(jobs))
-	threads,results=[],[]
-	for WorkerId in range(nthreads):
-		thread=MyThread()
-		threads.append(thread)
-		thread.jobs=[job for I,job in enumerate(jobs)  if (I % nthreads)==WorkerId]
-		thread.jobDone=lambda result: results.append(result)
-		thread.start()
-			
-	while True:
-
-		time.sleep(0.01)
-
-		if len(results)==len(jobs):
-			[thread.join() for thread in threads]
-			return results
-
-		if advance_callback:
-				advance_callback(len(results))
+# /////////////////////////////////////////
+def RemoveTree(dir):
+	while os.path.isdir(dir):
+		try:
+			shutil.rmtree(dir, ignore_errors=False)
+		except:
+			logger.info(f"Failed to removed directory {dir}, retrying in feww seconds")
+			time.sleep(1)
+	logger.info(f"Removed directory {dir}")
 
 
 # /////////////////////////////////////////////////////////////////////////
@@ -212,3 +268,119 @@ def TryRemoveFiles(mask):
 			os.remove(filename)
 		except:
 			pass
+
+
+# ////////////////////////////////////////////////////////////////////////
+def RunShellCommand(cmd, verbose=False, nretry=3):
+	logger.info(f"RunShellCommand {cmd} ...")
+	args=shlex.split(cmd)
+	
+	t1 = time.time()
+
+	for I in range(nretry):
+
+		result=subprocess.run(args, 
+			shell=False, 
+			check=False,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.STDOUT)
+
+		output=result.stdout.decode('utf-8')
+
+		if verbose: 
+			logger.info(output)
+
+		if result.returncode==0:
+			break
+
+		error_msg=f"RunShellCommand {args} failed with returncode={result.returncode} output:\n{output}"
+
+		if verbose or I==(nretry-1):
+			logger.info(error_msg)
+
+		if I==(nretry-1):
+			raise Exception(error_msg)
+
+	sec=time.time()-t1
+	logger.info(f"RunShellCommand {args} done in {sec} seconds")
+
+# ///////////////////////////////////////////////////
+class WorkerPool:
+
+	"""
+	We could use python ThreadPool for this, but you cannot submit jobs while running other jobs.
+	This class should resolve this problem, so you can run jobs, add new jobs while running and iterate
+	in results
+	"""
+    
+	# ________________________________________________
+	class WorkerClass:
+ 
+		def runWorkerLoop(self, worker_pool):
+			while True:
+				task=worker_pool.popTask()
+				if task is None: return # finished
+				result=task()
+				worker_pool.finishedTask(task, result)
+
+	class LastResult: pass
+
+	# constructor
+	def __init__(self,num_workers, worker_class=None):
+		if not worker_class:
+			worker_class=WorkerPool.WorkerClass
+		self.lock=multiprocessing.Lock()
+		self.processing=0
+		self.q=queue.Queue()
+		self.results=queue.Queue()
+		self.t1=time.time()	
+		self.exit=0
+		self.threads=[]
+		for n in range(num_workers):
+			worker = worker_class()
+			thread = threading.Thread(name=f"worker-{n:02d}",target=functools.partial(worker.runWorkerLoop,self))
+			self.threads.append(thread)
+
+	# start
+	def start(self):
+		for worker in self.threads:
+			worker.start()
+
+	# __iter__
+	def __iter__(self):
+		return self
+    
+	# __next__
+	def __next__(self):
+		ret=self.results.get()
+		if isinstance(ret,WorkerPool.LastResult):
+			self.exit=True
+
+			for thread in self.threads:
+				self.q.put(None) # tell worker to quit
+    
+			for thread in self.threads:
+				thread.join()
+    
+			raise StopIteration
+		else:
+			return ret
+
+	# pushTask
+	def pushTask(self, task):
+		with self.lock: 
+			self.q.put(task)
+			self.processing+=1
+   
+  # popTask
+	def popTask(self):
+		return self.q.get()
+
+	# finishedTask
+	def finishedTask(self, task, result):
+		with self.lock: 
+			self.processing-=1
+			self.results.put(result)
+			# if not processing add END result
+			if not self.processing:
+				self.results.put(WorkerPool.LastResult())    
