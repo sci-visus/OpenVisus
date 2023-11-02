2bf97b3ae49f67e94b8b18a12b33588f58798083
Giorgio Scorzelli <scrgiorgio@gmail.com>
2023-11-02 15:23:03 +0100

    New tag (2.2.81)

--- scripts/new-tag.sh
+++ scripts/new-tag.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-TAG=$(python3 Libs/swig/setup.py new-tag) && echo ${TAG}
+# TAG=$(python3 Libs/swig/setup.py new-tag) && echo ${TAG}
+TAG=2.2.81
 
 git commit -a -m "New tag ($TAG)" 
 git tag -a $TAG -m "$TAG"
