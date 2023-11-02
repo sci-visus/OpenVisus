9956ef94022c5da37ecdef4adc0319d80b371041
Giorgio Scorzelli <scrgiorgio@gmail.com>
2023-11-02 15:23:38 +0100

    New tag (2.2.127)

--- Libs/swig/setup.py
+++ Libs/swig/setup.py
@@ -6,7 +6,7 @@ try:
 except:
 	pass
 
-PROJECT_VERSION="2.2.126"
+PROJECT_VERSION="2.2.127"
 
 this_dir=os.path.dirname(os.path.abspath(__file__))
 
--- scripts/new-tag.sh
+++ scripts/new-tag.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-# TAG=$(python3 Libs/swig/setup.py new-tag) && echo ${TAG}
-TAG=2.2.81
+TAG=$(python3 Libs/swig/setup.py new-tag) && echo ${TAG}
 
 git commit -a -m "New tag ($TAG)" 
 git tag -a $TAG -m "$TAG"
