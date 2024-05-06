17c28138ee9d44fa67d361d332d7dbb3e3a563d9
Giorgio Scorzelli <scrgiorgio@gmail.com>
2024-05-06 19:57:36 +0200

    Adding IDX2

--- Libs/IDX2/Source/Core/InputOutput.h
+++ Libs/IDX2/Source/Core/InputOutput.h
@@ -45,14 +45,13 @@ DumpText(cstr FileName, i Begin, i End, cstr Format);
 #elif defined(__CYGWIN__) || defined(__linux__) || defined(__APPLE__)
 #define _FILE_OFFSET_BITS 64
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdlib.h>
-
+#if defined(__APPLE__)
+#define idx2_FSeek fseek
+#define idx2_FTell ftell
+#else
 #define idx2_FSeek fseeko
 #define idx2_FTell ftello
+#endif
 
 #endif
 
