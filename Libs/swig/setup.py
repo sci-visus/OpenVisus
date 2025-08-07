706eed78d94ec7bb5b86c57c4c0045dc1382a03d
Giorgio Scorzelli <scrgiorgio@gmail.com>
2025-08-07 13:56:20 -0600

    New tag

--- Libs/swig/setup.py
+++ Libs/swig/setup.py
@@ -7,7 +7,7 @@ except:
 	pass
 
 # the following line can be replaced automatically by `NewTag` so be careful not to change its format
-PROJECT_VERSION="2.2.141"
+PROJECT_VERSION="2.2.142"
 
 # I can override by env variable
 if "PROJECT_VERSION" in os.environ:
